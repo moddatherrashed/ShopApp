c8e17428060305a49618b74beb3b611de8ea4e17
moddather rashed <moddather.developer@gmail.com>
2018-02-19 20:01:41 +0000

    add styling

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -12,27 +12,44 @@ class HomeScreen extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            data: []
+            data: [
+                {
+                    name: 'Gray Jacket',
+                    url: { uri: 'https://4fstore.com/gfx/1510748446.4518.jpg' }
+                },
+                {
+                    name: 'Adidas Jacket',
+                    url: { uri: 'http://www.sportzone.sk/img/cache/public/f1-w800-h800-r-b255-255-255-o-f1/photos/57/566/56517.jpg' }
+                },
+                {
+                    name: 'Water Prof Jacket',
+                    url: { uri: 'http://www.blackhoodies.co.uk/image/cache/catalog/BLKREDTS011-540x720.jpg' }
+                },
+                {
+                    name: 'White T-shirt',
+                    url: { uri: 'https://4f.com.pl/gfx/big/1508938910.8935.jpg' }
+                }],
+            offerFalg: true
         }
     }
     static navigationOptions = {
         drawerIcon: (
             <Icon name='home' style={{ color: '#FFFFFF' }} />
         ),
-        title: `Home`,
-        headerTintColor: '#363A57',
+        title: `Catagories`,
+        headerTintColor: '#FFFFFF',
         headerStyle: {
-            backgroundColor: '#FFFFFF',
+            backgroundColor: '#363A57',
         },
         headerRight: <Button transparent
             onPress={() => _this.searchHeader.show()}>
-            <Icon name='cart' style={{ color: '#363A57' }} />
+            <Icon name='cart' style={{ color: '#FFFFFF' }} />
         </Button>,
         headerLeft: <Button transparent
             onPress={() => {
                 _this.props.navigation.navigate('DrawerOpen')
             }}>
-            <Icon name='menu' style={{ color: '#363A57' }} />
+            <Icon name='menu' style={{ color: '#FFFFFF' }} />
         </Button>
     }
 
@@ -43,8 +60,36 @@ class HomeScreen extends Component {
     onNavigate(context, params) {
         context.props.navigation.navigate('CatagoryScreen', params)
     }
+    renderOffer(offerFalg) {
+        if (offerFalg) {
+            return (
+                <View style={{
+                    borderWidth: 1, borderColor: '#FFFFFF', margin: 5, backgroundColor: '#FFFFFF',
+                    elevation: 15, borderRadius: 5
+                }}>
+                    <Image
+                        style={{ height: ScreenSize.height * 0.4, borderRadius: 5, width: '100%' }}
+                        source={{ uri: 'http://s1.picswalls.com/wallpapers/2014/07/24/sports-desktop-background_113128110_89.jpg' }}
+                        resizeMode="cover"
+                    />
+                </View>
+            )
+        }
+    }
     render() {
         const { navigate } = this.props.navigation
+        let width = ScreenSize.width
+        let height = ScreenSize.height
+        if (width > height) {
+            //tablet
+            width = height * 0.37
+            colNum = 3
+
+        } else {
+            //normal phone
+            width = width * 0.37
+            colNum = 2
+        }
         return (
 
             <Container>
@@ -58,26 +103,44 @@ class HomeScreen extends Component {
                         }}
                         headerBgColor='#363A57'
                     />
-                    <ImageBackground
-                        source={{ uri: 'http://www.tokkoro.com/picsup/3063936-blue-jeans_blur_clothes_cold_couple_fashion_girl_jacket_leisure_love_man_model_outdoors_path_pathway_pavement_people_photoshoot_romance_romantic_scarf_shoes_style_stylish_together_togetherness_wa.jpg' }}
-                        style={{
-                            height: ScreenSize.height * 0.91,
-                            width: '100%',
-                            justifyContent: 'center'
-                        }}>
-                        <Button full style={styles.ButtonStyle}
-                            onPress={() => this.onNavigate(this, { name: 'Women' })} >
-                            <Text style={styles.textStyle}>Women</Text>
-                        </Button>
-                        <Button full style={styles.ButtonStyle}
-                            onPress={() => this.onNavigate(this, { name: 'Man' })}>
-                            <Text style={styles.textStyle}>Man</Text>
-                        </Button>
-                        <Button full style={styles.ButtonStyle}
-                            onPress={() => this.onNavigate(this, { name: 'Accessoies' })} >
-                            <Text style={styles.textStyle}>Accessoies</Text>
-                        </Button>
-                    </ImageBackground>
+                    {this.renderOffer(this.state.offerFalg)}
+                    <FlatList
+                        contentContainerStyle={{ margin: 2 }}
+                        horizontal={false}
+                        numColumns={colNum}
+                        keyExtractor={item => item.name}
+                        contentContainerStyle={{
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}
+                        data={this.state.data}
+                        renderItem={({ item, index }) =>
+                            <View>
+                                <TouchableOpacity
+                                    style={{
+                                        flex: 1,
+                                        margin: 5,
+                                        height: width * 2,
+                                        width: width * 1.28,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        borderColor: '#FFFFFF',
+                                        borderWidth: 1,
+                                        elevation: 15,
+                                        backgroundColor: '#FFFFFF'
+                                    }}
+                                    onPress={()=> navigate('CatagoryScreen' , {name : item.name})}>
+                                    <Image
+                                        style={{ height: '80%', width: '100%', margin: 3 }}
+                                        source={item.url}
+                                        resizeMode="contain"
+                                    />
+                                    <Text style={styles.textStyle}>{item.name}</Text>
+                                </TouchableOpacity>
+
+                            </View>
+                        }
+                    />
                 </Content>
             </Container>
         )
