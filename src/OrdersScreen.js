85097fd6c928763ce0bef2355691dca0b8ad3f98
moddatherrashed <moddather.developer@gmail.com>
2018-03-18 19:40:38 +0000

    Add onOrdersEmpty indecator to detect if there is no orders

--- src/OrdersScreen.js
+++ src/OrdersScreen.js
@@ -9,27 +9,27 @@ class AccountScreen extends Component {
         super(props)
         this.state = {
             orders: [{
-                name: 'Gray Jacket',
+                name: '11111',
                 url: { uri: 'https://4fstore.com/gfx/1510748446.4518.jpg' },
                 date: '02-06-2018'
             },
             {
-                name: 'Adidas Jacket',
+                name: '22222',
                 url: { uri: 'http://www.blackhoodies.co.uk/image/cache/catalog/BLKREDTS011-540x720.jpg' },
                 date: '02-06-2018'
             },
             {
-                name: 'Water Prof Jacket',
+                name: '33333',
                 url: { uri: 'http://www.blackhoodies.co.uk/image/cache/catalog/BLKREDTS011-540x720.jpg' },
                 date: '02-06-2018'
             },
             {
-                name: 'White T-shirt',
+                name: '444444',
                 url: { uri: 'https://4f.com.pl/gfx/big/1508938910.8935.jpg' },
                 date: '02-06-2018'
             },
             {
-                name: 'Gray Jacket',
+                name: '5555555',
                 url: { uri: 'https://4fstore.com/gfx/1510748446.4518.jpg' },
                 date: '02-06-2018'
             },
@@ -56,6 +56,65 @@ class AccountScreen extends Component {
             <Icon name='list' style={{ color: '#FFFFFF' }} />
         )
     }
+
+    renderList() {
+        if (this.state.orders.length !== 0) {
+            return (
+                <FlatList
+                    contentContainerStyle={{ margin: 2 }}
+                    keyExtractor={item => item.name}
+                    contentContainerStyle={{
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
+                    data={this.state.orders}
+                    renderItem={({ item, index }) =>
+                        <View style={{
+                            flexDirection: 'row',
+                            backgroundColor: '#FFFFFF',
+                            elevation: 15,
+                            width: '100%',
+                            borderBottomWidth: 0.5,
+                            borderBottomColor: 'gray'
+                        }}>
+                            <View style={{ flexDirection: 'row', flex: 6 }}>
+                                <View style={{ flex: 2 }}>
+                                    <Image
+                                        style={{ height: 100, width: 100, margin: 5 }}
+                                        source={item.url}
+                                        resizeMode='contain'
+                                    />
+                                </View>
+                                <View style={{ flex: 3, justifyContent: 'center', alignItems: 'flex-start' }}>
+                                    <Text style={{ color: '#363A57' }}>name: {item.name}</Text>
+                                    <Text style={{ color: '#363A57' }}>ordered in: {item.date}</Text>
+                                    <Text style={{ fontWeight: 'bold', color: '#363A57' }}>50 JD</Text>
+                                </View>
+                                <View style={{ flex: 1, alignItems: 'flex-end' }}>
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            let restOfOrders = this.state.orders
+                                            restOfOrders.splice(index, 1)
+                                            this.setState({
+                                                orders: restOfOrders
+                                            })
+                                        }}>
+                                        <Icon name='ios-close' style={{ color: '#363A57', padding: 10 }} />
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                    }
+                />
+            )
+        } else {
+            return (
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                    <Text style={{ fontWeight: 'bold', marginTop: '50%', fontSize: 25 }}>you have no orders yet</Text>
+                </View>
+            )
+        }
+    }
     render() {
         return (
             <Container>
@@ -80,45 +139,7 @@ class AccountScreen extends Component {
                 </Header>
                 <Content>
                     <View>
-                        <FlatList
-                            contentContainerStyle={{ margin: 2 }}
-                            keyExtractor={item => item.name}
-                            contentContainerStyle={{
-                                justifyContent: 'center',
-                                alignItems: 'center'
-                            }}
-                            data={this.state.orders}
-                            renderItem={({ item, index }) =>
-                                <View style={{
-                                    flexDirection: 'row',
-                                    backgroundColor: '#FFFFFF',
-                                    elevation: 15,
-                                    width: '100%',
-                                    borderBottomWidth: 0.5,
-                                    borderBottomColor: 'gray'
-                                }}>
-                                    <View style={{ flexDirection: 'row', flex: 6 }}>
-                                        <View style={{ flex: 2 }}>
-                                            <Image
-                                                style={{ height: 100, width: 100, margin: 5 }}
-                                                source={item.url}
-                                                resizeMode='contain'
-                                            />
-                                        </View>
-                                        <View style={{ flex: 3, justifyContent: 'center', alignItems: 'flex-start' }}>
-                                            <Text style={{ color: '#363A57' }}>name: {item.name}</Text>
-                                            <Text style={{ color: '#363A57' }}>ordered in: {item.date}</Text>
-                                            <Text style={{ fontWeight: 'bold', color: '#363A57' }}>50 JD</Text>
-                                        </View>
-                                        <View style={{ flex: 1, alignItems: 'flex-end' }}>
-                                            <TouchableOpacity>
-                                                <Icon name='ios-close' style={{ color: '#363A57', padding: 10 }} />
-                                            </TouchableOpacity>
-                                        </View>
-                                    </View>
-                                </View>
-                            }
-                        />
+                        {this.renderList()}
                     </View>
                 </Content>
             </Container>
