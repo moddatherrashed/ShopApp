3b982137772df2c368cb2e65b20afebb3added6c
moddatherrashed <moddather.developer@gmail.com>
2018-04-04 23:51:14 +0100

    make language screen ready

--- src/LanguageScreen.js
+++ src/LanguageScreen.js
@@ -5,6 +5,16 @@ import { StackNavigator } from 'react-navigation'
 
 
 class LanguageScreen extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            arabicBtnBackground: '#FFFFFF',
+            englsihBtnBackground: '#363A57',
+            arabicTextColor: '#363A57',
+            englishTextColor: '#FFFFFF',
+        }
+    }
     static navigationOptions = {
         headerTitle: `Language Selector`,
         headerTintColor: '#FFFFFF',
@@ -34,17 +44,69 @@ class LanguageScreen extends Component {
         ),
     }
 
+    languageSelector(lang) {
+        if (lang == 'ar') {
+            this.setState({
+                arabicTextColor: '#FFFFFF',
+                arabicBtnBackground: '#363A57',
+                englsihBtnBackground: '#FFFFFF',
+                englishTextColor: '#363A57',
+            })
+        } else {
+            this.setState({
+                englsihBtnBackground: '#363A57',
+                englishTextColor: '#FFFFFF',
+                arabicTextColor: '#363A57',
+                arabicBtnBackground: '#FFFFFF',
+            })
+        }
+    }
+
     render() {
         return (
-            <Container>
+
+            <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
                 <StatusBar
                     barStyle="light-content"
                     backgroundColor="#4d537c"
                     hidden={false} />
-                <Content>
-                    <Text>here is language Screen</Text>
-                </Content>
-            </Container>
+                <Text style={{ margin: 20, fontSize: 20 }}>Choose your Language:</Text>
+
+                <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                    <Button bordered style={{
+                        backgroundColor: this.state.englsihBtnBackground,
+                        borderColor: '#363A57',
+                        height: 100,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        margin: 20,
+                        flex: 1
+                    }}
+                        onPress={() => { this.languageSelector('en') }}>
+                        <Text style={{
+                            color: this.state.englishTextColor,
+                            fontWeight: 'bold',
+                            fontSize: 30
+                        }} >English</Text>
+                    </Button>
+                    <Button bordered style={{
+                        backgroundColor: this.state.arabicBtnBackground,
+                        height: 100,
+                        borderColor: '#363A57',
+                        margin: 20,
+                        justifyContent: 'center',
+                        flex: 1
+                    }}
+                        onPress={() => { this.languageSelector('ar') }}>
+                        <Text style={{
+                            color: this.state.arabicTextColor,
+                            fontWeight: 'bold',
+                            fontSize: 30
+                        }} >العربية</Text>
+                    </Button>
+                </View>
+            </View>
+
         )
     }
 }
